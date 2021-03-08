bd807f7da05c2017ae0c1f685d09e0ebdcd03c1a
Thomas Chaplin <thomas.chaplin@tray.io>
2021-03-08 21:06:47 +0000

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use exitfailure::ExitFailure;
+use failure::ResultExt;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+    print_content(&content);
+
+    Ok(())
+}
+
+fn print_content(content: &str) -> () {
+    for line in content.lines() {
+            println!("{}", line);
+    }
+}
